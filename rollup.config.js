73af50724f588ec8105a7da84f656db0dc531bde
Jaider-Uribe <128413541+Jaider-Uribe@users.noreply.github.com>
2023-12-18 09:47:00 -0500

    Se configura rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-import { babel } from '@rollup/plugin-babel';
 
 const packageJson = require("./package.json");
 
 export default [
-  // Step 1
   {
     input: "src/index.ts",
     output: [
@@ -24,18 +21,14 @@ export default [
       },
     ],
     plugins: [
-      resolve({extensions: ['.js', '.ts']}),
-      babel({ babelHelpers: 'bundled', extensions: [".js", ".jsx"], }),
+      resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
     ],
   },
-  // Step 2.
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.(css|less|scss)$/],
   },
 ];
